fbfe652389de20a8c9deec94cb8d2fc22c84ab67
hrdzkj <1017965668@qq.com>
2017-06-22 11:08:46 +0800

    Update envpara.sh

--- download/envpara.sh
+++ download/envpara.sh
@@ -1,5 +1,5 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export HADOOP_HOME=/opt/hadoop-2.6.0
+export HADOOP_HOME=/opt/hadoop-2.4.0
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
